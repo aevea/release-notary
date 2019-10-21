21c93aaa63686e3e8a3b929270824e994d09fc42
Simon Prochazka <simon@fallion.net>
2019-10-21 12:52:32 +0200

    fix: improved tracking for repositories with detached tags
    - adds multiple tests for different edge cases
    - sets up scripts for creating testing git repositories

---
+++ cmd/get_commits_test.go
@@ -0,0 +1,48 @@
+package cmd
+
+import (
+	"testing"
+
+	history "github.com/commitsar-app/git/pkg"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetCommitsDetachedTags(t *testing.T) {
+	repo, err := history.OpenGit("../testdata/detached_tag", false)
+
+	assert.NoError(t, err)
+
+	commits, err := getCommits(repo)
+
+	assert.NoError(t, err)
+	assert.Equal(t, 2, len(commits))
+
+	commitObject, err := repo.Commit(commits[0])
+
+	assert.NoError(t, err)
+	assert.Equal(t, "feat: third file\n", commitObject.Message)
+
+	commitObject, err = repo.Commit(commits[1])
+	assert.NoError(t, err)
+	assert.Equal(t, "feat: fourth file\n", commitObject.Message)
+}
+
+func TestGetCommitsSingleBranch(t *testing.T) {
+	repo, err := history.OpenGit("../testdata/single_branch_tags", false)
+
+	assert.NoError(t, err)
+
+	commits, err := getCommits(repo)
+
+	assert.NoError(t, err)
+	assert.Equal(t, 2, len(commits))
+
+	commitObject, err := repo.Commit(commits[0])
+
+	assert.NoError(t, err)
+	assert.Equal(t, "feat: third file\n", commitObject.Message)
+
+	commitObject, err = repo.Commit(commits[1])
+	assert.NoError(t, err)
+	assert.Equal(t, "feat: second file\n", commitObject.Message)
+}
