9ba65c7e9090528cc0086cdfe3013eac4c3c6d8d
Simon Prochazka <simon@fallion.net>
2019-10-23 00:15:26 +0200

    fix: parse commit body
    - commit body was not being parsed by ParseCommitMessage
    - simplifies the logic for parsing the commit to use named groups instead of using match indexes like before

--- internal/text/parse_commits_test.go
+++ internal/text/parse_commits_test.go
@@ -9,10 +9,10 @@ import (
 func TestParseCommitMessage(t *testing.T) {
 
 	tests := map[string]Commit{
-		"chore: testing\n":                  Commit{Category: "chore", Scope: "", Heading: "testing"},
-		"feat(ci): ci test\n":               Commit{Category: "feat", Scope: "ci", Heading: "ci test"},
-		"merge master in something\n":       Commit{Category: "other", Scope: "", Heading: "merge master in something\n"},
-		"chore: test\n something more here": Commit{Category: "chore", Scope: "", Heading: "test"},
+		"chore: testing\n":                   Commit{Category: "chore", Scope: "", Heading: "testing"},
+		"feat(ci): ci test\n":                Commit{Category: "feat", Scope: "ci", Heading: "ci test"},
+		"merge master in something\n":        Commit{Category: "other", Scope: "", Heading: "merge master in something\n"},
+		"chore: test\n\nsomething more here": Commit{Category: "chore", Scope: "", Heading: "test", Body: "something more here"},
 	}
 
 	for test, expected := range tests {
