9e30b40cdaa35dcc46d1cbfbeb1114b075e0ef06
Simon Prochazka <simon@fallion.net>
2019-10-22 16:18:06 +0200

    chore!: rework release notes to use maps
    BREAKING CHANGE: Sections struct has been removed. 
    
    - maps are now used instead. This is a pre-requisite for dynamic mappings using a config file.

--- internal/text/release_notes.go
+++ internal/text/release_notes.go
@@ -3,25 +3,25 @@ package text
 import "strings"
 
 // ReleaseNotes generates the output mentioned in the expected-output.md
-func ReleaseNotes(sections Sections) string {
+func ReleaseNotes(sections map[string][]Commit) string {
 	builder := strings.Builder{}
 	// Extra lines at the start to make sure formatting starts correctly
 	builder.WriteString("\n\n")
 
-	if len(sections.Features) > 0 {
-		builder.WriteString(buildSection("features", sections.Features))
+	if len(sections["features"]) > 0 {
+		builder.WriteString(buildSection("features", sections["features"]))
 	}
 
-	if len(sections.Bugs) > 0 {
-		builder.WriteString(buildSection("bugs", sections.Bugs))
+	if len(sections["bugs"]) > 0 {
+		builder.WriteString(buildSection("bugs", sections["bugs"]))
 	}
 
-	if len(sections.Chores) > 0 {
-		builder.WriteString(buildSection("chores", sections.Chores))
+	if len(sections["chores"]) > 0 {
+		builder.WriteString(buildSection("chores", sections["chores"]))
 	}
 
-	if len(sections.Others) > 0 {
-		builder.WriteString(buildSection("others", sections.Others))
+	if len(sections["others"]) > 0 {
+		builder.WriteString(buildSection("others", sections["others"]))
 	}
 
 	return builder.String()
