591e9e59d274ac60f1d0f810ddeda43cd330c77a
Simon Prochazka <simon@fallion.net>
2019-10-23 00:15:26 +0200

    fix: parse commit body
    - commit body was not being parsed by ParseCommitMessage
    - simplifies the logic for parsing the commit to use named groups instead of using match indexes like before

--- internal/text/parse_commits.go
+++ internal/text/parse_commits.go
@@ -6,28 +6,31 @@ import (
 )
 
 var (
-	expectedFormatRegex = regexp.MustCompile(`^(?P<type>\S+?)(?P<scope>\(\S+\)?)?!?:\s(?P<message>.+)\n`)
+	expectedFormatRegex = regexp.MustCompile(`(?s)^(?P<category>\S+?)?(?P<scope>\(\S+\))?(?P<breaking>!?)?: (?P<heading>[^\n\r]+)?([\n\r]{2}(?P<body>.*))?`)
 )
 
 // ParseCommitMessage creates a slice of Commits that contain information about category and scope parsed from commit message
 func ParseCommitMessage(commitMessage string) Commit {
 	match := expectedFormatRegex.FindStringSubmatch(commitMessage)
+
 	if len(match) > 0 {
-		// in case of no scope
-		category := match[1]
-		message := match[2]
-		scope := ""
+		result := make(map[string]string)
 
-		// if 2nd match is found then scope is present
-		if match[3] != "" {
-			message = match[3]
-			scope = match[2]
-
-			scope = strings.Replace(scope, "(", "", 1)
-			scope = strings.Replace(scope, ")", "", 1)
+		for i, name := range expectedFormatRegex.SubexpNames() {
+			if i != 0 && name != "" {
+				result[name] = match[i]
+			}
 		}
 
-		return Commit{Category: category, Heading: message, Scope: scope}
+		category := result["category"]
+		scope := result["scope"]
+		heading := result["heading"]
+		body := result["body"]
+
+		scope = strings.Replace(scope, "(", "", 1)
+		scope = strings.Replace(scope, ")", "", 1)
+
+		return Commit{Category: category, Heading: heading, Scope: scope, Body: body}
 	}
 
 	return Commit{Category: "other", Heading: commitMessage, Scope: ""}
