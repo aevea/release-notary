13c196ba69e6936cd8564eacc8e457c29c511091
Alex Viscreanu <alexviscreanu@gmail.com>
2019-11-27 13:24:15 +0100

    fix(publish): Incomplete changelog for non-compliant commits
    Also...
    
    * Simplified tests
    * Simplified .gitignore
    * Automated setup_test_repos.sh script

--- testdata/setup_test_repos.sh
+++ testdata/setup_test_repos.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-rm -rf testdata/detached_tag
-rm -rf testdata/single_branch_tags
-
-cd testdata
-
-echo 'testdata/detached_tag/ directory does not exist at the root; creating...'
-git clone detached_tag.bundle
-echo 'done'
-
-echo 'testdata/detached_tag/ directory does not exist at the root; creating...'
-git clone single_branch_tags.bundle
-echo 'done'
+for bundle in *.bundle; do
+  repo="${bundle%.*}"
+  rm -rf ./$repo;
+  echo "testdata/$repo directory does not exist at the root; creating...";
+  git clone $bundle;
+  echo "done";
+done
